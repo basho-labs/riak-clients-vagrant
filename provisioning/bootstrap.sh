4536928b7f0b7a2c6ece1d14a8bc0c4b506c0db2
Christopher Mancini <cmancini@basho.com>
2016-12-12 16:44:12 -0500

    Tweak bootstrap.sh to show the ansible-playbook command being executed. simplify the if conditional. Remove dupe connection statement.

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -33,40 +33,40 @@ printf '
 ##                                                                                  ##
 ##             Executing Ansible playbook, please be patient...                     ##
 ##                                                                                  ##
-######################################################################################'
+######################################################################################
+'
+
 
 if [[ "$1" == *"kv"* ]]
 then
-	printf 'Running KV tests \n'
-    ansible-playbook /vagrant/provisioning/playbook.yml --extra-vars="${2}"
+    PLAYBOOK='playbook'
 elif [[ "$1" == *"ts"* ]]
 then
-	printf 'Running TS tests \n'
-    ansible-playbook /vagrant/provisioning/timeseries.yml --extra-vars="${2}"
+    PLAYBOOK='timeseries'
 elif [[ "$1" == *"security"* ]]
 then
-	printf 'Running Security tests \n'
-    ansible-playbook /vagrant/provisioning/security.yml --extra-vars="${2}"
+    PLAYBOOK='security'
 else
-	printf 'Defaulting to running KV tests \n'
-    ansible-playbook /vagrant/provisioning/playbook.yml --extra-vars="${2}"
+    PLAYBOOK='playbook'
 fi
+
+ansible-playbook /vagrant/provisioning/${PLAYBOOK}.yml --extra-vars="${2}"
 
 printf '
 ######################################################################################
 ##                                                                                  ##
 ##                          Provisioning complete!                                  ##
 ##                                                                                  ##
-## If you need to run the main playbook manually, use:                              ##
-##   `sudo ansible-playbook /vagrant/provisioning/playbook.yml`                     ##
-##                                                                                  ##
 ## Useful commands:                                                                 ##
 ##   - `vagrant ssh` login to your VM via ssh                                       ##
 ##   - `vagrant provision` rerun this provisioner                                   ##
 ##                                                                                  ##
-## If you see red (error) above:                                                    ##
+## If you see errors above:                                                         ##
 ##   - Try vagrant destroy, then vagrant up again                                   ##
 ##   - If you cannot resolve it, please copy / pasta the error to a GitHub Issue    ##
 ##                                                                                  ##
+## If you need to run the playbook manually, use:                                   ##
+##   `sudo ansible-playbook /vagrant/provisioning/%s.yml --extra-vars="%s"`         ##
+##                                                                                  ##
 ######################################################################################
-'
+' ${PLAYBOOK} "${2}"
